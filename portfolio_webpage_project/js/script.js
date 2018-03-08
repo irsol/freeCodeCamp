96201cf2a3e7b3aadae3d87609a37f470fc56a7d
irsol <irinywka@gmail.com>
2018-03-08 17:58:01 +0100

    Add parallax to html

--- portfolio_webpage_project/js/script.js
+++ portfolio_webpage_project/js/script.js
@@ -1,3 +1,10 @@
 $('#navbar li').click(function(){
 	$(this).addClass('active').siblings().removeClass('active');
 });
+
+
+
+$('.parallax-window').parallax({
+    naturalWidth: 600,
+    naturalHeight: 400
+  });
